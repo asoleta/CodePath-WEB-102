f520464bb98ce0e823394ae8260345627fbc3131
Alex Soleta <alexandrasoleta@gmail.com>
2024-10-15 21:48:46 -0400

    Complete lab

--- Lab 5 - Cryptocurrency/crypto-hustle/src/App.jsx
+++ Lab 5 - Cryptocurrency/crypto-hustle/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+import './App.css';
+import CoinInfo from './components/CoinInfo';
+import SideNav from './components/SideNav';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) => 
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      try {
+        const response = await fetch(
+          "https://min-api.cryptocompare.com/data/all/coinlist?api_key=" + API_KEY
+        );
+        const json = await response.json();
+        setList(json);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    
+    fetchAllCoinData();
+  }, []);
+
+  console.log(list?.Data); // Safely check list.Data
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+      <ul>
+        {searchInput.length > 0
+        ? filteredResults.map((coin) => 
+            list.Data[coin].PlatformType === "blockchain" ? 
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+            : null
+          )
+        : list && Object.entries(list.Data).map(([coin]) => 
+            list.Data[coin].PlatformType === "blockchain" ? 
+            <CoinInfo
+              image={list.Data[coin].ImageUrl}
+              name={list.Data[coin].FullName}
+              symbol={list.Data[coin].Symbol}
+            />
+        : null
+      )}
+      </ul>
+    </div>
+    <SideNav />
     </>
-  )
+    
+  );
 }
 
-export default App
+export default App;
